72bb82fbf8fb25a61ea6932e622bbb56f41ba429
gueg <gdamien56@gmail.com>
2013-06-28 11:06:40 +0200

    ajout du script

---
+++ listing.ps1
@@ -0,0 +1,9 @@
+﻿#Users into CSV file
+#adding in a csv file
+#ajout des recherches dans un fichier csv
+$FilePath = "c:\people3.csv" 
+$OuDomain = "OU=Damien,DC=test,DC=local,DC=damien,DC=fr"
+Get-QADUser -sl 0 -searchRoot $OuDomain | select-Object Firstname, Mail, Description, Address, SamAccountName, Displayname, Location, logonscrip, Company, title, department, telephonenumber | Export-Csv $FilePath
+
+
+
